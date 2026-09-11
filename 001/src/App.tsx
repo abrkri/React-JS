45e78d713fc2385d92ccd7f52b8f34dc78786612
Apatej <abrahamkristof9@gmail.com>
2026-09-11 11:33:44 +0200

    cucclik

--- 001/src/App.tsx
+++ 001/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
 const App = () => {
+  const nev: string = "Pisti";
   return (
     <>
-      <p>szoveg</p>
-      <p>szoveg</p>
+      <h1>szoveg</h1>
+      <p>
+        Az en nevem: <b>{nev}</b>
+      </p>
     </>
   );
 };
