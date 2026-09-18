981bf6854703f3a3b3dc3a1806a7ff4b2b787e41
Apatej <abrahamkristof9@gmail.com>
2026-09-18 09:15:42 +0200

    gombok masik

--- 006/src/App.tsx
+++ 006/src/App.tsx
@@ -1,18 +1,29 @@
 import "./App.css";
 
 function App() {
+  const submit = () => {
+    alert("Megnyomva 3!");
+  };
+
+  const submit2 = (szoveg: string) => {
+    alert("Megnyomva 5!");
+  };
+
   return (
     <>
       <h1>Csaocsaocsao</h1>
 
-      <button onClick={() => alert("Megnyomva 1!")}>Nyomj meg</button>
+      <button onClick={() => alert("Megnyomva 1!")}>Nyomj meg 1</button>
       <button
         onClick={() => {
           alert("Megnyomva 2!");
         }}
       >
-        Nyomj meg
+        Nyomj meg 2
       </button>
+      <button onClick={() => submit()}>Nyomj meg 3</button>
+      <button onClick={submit}>Nyomj meg 4</button>
+      <button onClick={() => submit2("Megnyomva 5!")}>Nyomj meg 5</button>
     </>
   );
 }
