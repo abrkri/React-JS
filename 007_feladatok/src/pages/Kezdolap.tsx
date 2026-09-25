c86817d9843e5154458a7d5f47375f5854f99f6a
Apatej <abrahamkristof9@gmail.com>
2026-09-25 11:32:55 +0200

    gombok a kezdolapba A taggel

--- 007_feladatok/src/pages/Kezdolap.tsx
+++ 007_feladatok/src/pages/Kezdolap.tsx
@@ -2,6 +2,18 @@ const Kezdolap = () => {
   return (
     <>
       <h1>Ez itt a kezdőlap</h1>
+      <a href="/homerseklet">
+        <button>Hőmérséklet</button>
+      </a>
+      <a href="/szamologep">
+        <button>Számológép</button>
+      </a>
+      <a href="/bmi">
+        <button>BMI kalkulátor</button>
+      </a>
+      <a href="/penzszamito">
+        <button>Pénzszámító</button>
+      </a>
     </>
   );
 };
